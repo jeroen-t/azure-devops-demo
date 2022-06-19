d6a437f465d5b215c0fcda13c11186c1c554574a
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2022-06-19 21:04:47 +0200

    What about now

--- bin/main.go
+++ bin/main.go
@@ -5,8 +5,8 @@ import (
 	"os"
 )
 
-var SECRET_NAME string = os.Getenv("SECRET_NAME")
-var SECRET_VALUE string = os.Getenv("SECRET_VALUE")
+var SECRET_NAME string = os.Getenv("MY_SECRET_NAME")
+var SECRET_VALUE string = os.Getenv("MY_SECRET_VALUE")
 
 func main() {
 	fmt.Printf("name: %s\nvalue: %s\n", SECRET_NAME, SECRET_VALUE)
